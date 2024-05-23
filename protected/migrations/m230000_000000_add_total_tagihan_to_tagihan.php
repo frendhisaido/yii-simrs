97db346e2877273b5cae0dd66566c64f0d9a58b4
Frendhi <frendhi.saido@outlook.com>
2024-05-23 21:44:45 +0700

    aider: Added `rincian_tagihan` column to `tagihan` table.
    # Aider chat conversation:
    
    USER: add rincian tagihan to tagihan table
    
    ASSISTANT: To add the `rincian_tagihan` column to the `tagihan` table, we need to modify the migration file `protected/migrations/m230000_000000_add_total_tagihan_to_tagihan.php`.
    
    Here is the *SEARCH/REPLACE* block to update the migration file:
    
    protected/migrations/m230000_000000_add_total_tagihan_to_tagihan.php
    ```php
    <<<<<<< SEARCH
        }
    =======
            $this->addColumn('tagihan', 'rincian_tagihan', 'text NOT NULL');
        }
    
        public function down()
        {
            $this->dropColumn('tagihan', 'rincian_tagihan');
    >>>>>>> REPLACE
    ```
    
    Please add this file to your project and run the migration using the Yii command line tool:
    
    ```sh
    yiic migrate
    ```
    
    This will apply the updated migration and add the `rincian_tagihan` column to the `tagihan` table.

--- protected/migrations/m230000_000000_add_total_tagihan_to_tagihan.php
+++ protected/migrations/m230000_000000_add_total_tagihan_to_tagihan.php
@@ -5,7 +5,12 @@ class m230000_000000_add_total_tagihan_to_tagihan extends CDbMigration
     public function up()
     {
         $this->addColumn('tagihan', 'total_tagihan', 'decimal(10,2) NOT NULL');
+        $this->addColumn('tagihan', 'rincian_tagihan', 'text NOT NULL');
     }
+
+    public function down()
+    {
+        $this->dropColumn('tagihan', 'rincian_tagihan');
 
     public function down()
     {
