fd6ee01a9a2cbdd315e3b7a797b56393ea8cab1d
Frendhi <frendhi.saido@outlook.com>
2024-05-23 20:43:24 +0700

    Add view.php for pendaftaran module.

---
+++ protected/views/pendaftaran/view.php
@@ -0,0 +1,48 @@
+<?php
+/* @var $this PendaftaranController */
+/* @var $model Pendaftaran */
+
+$this->breadcrumbs=array(
+	'Pendaftarans'=>array('index'),
+	$model->id,
+);
+
+$this->menu=array(
+	array('label'=>'List Pendaftaran', 'url'=>array('index')),
+	array('label'=>'Create Pendaftaran', 'url'=>array('create')),
+	array('label'=>'Update Pendaftaran', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Pendaftaran', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Pendaftaran', 'url'=>array('admin')),
+);
+?>
+
+<h1>View Pendaftaran #<?php echo $model->id; ?></h1>
+
+<?php 
+	$this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		array(
+			'name'=>'user_id',
+			'value'=>$model->user->username,
+		),
+		array(
+			'name'=>'pasien_id',
+			'value'=>$model->pasien->nama
+		),
+		// Show obatPasiens if there are any
+		array(
+			'name'=>'obat',
+			//obatPasiens is an array of ObatPasien objects
+			'value'=>implode(', ', CHtml::listData($model->obatPasiens, 'id', 'obat.nama')),
+		),
+		// Show tindakanPasiens if there are any
+		array(
+			'name'=>'tindakan',
+			//tindakanPasiens is an array of TindakanPasien objects
+			'value'=>implode(', ', CHtml::listData($model->tindakanPasiens, 'id', 'tindakan.nama')),
+		),
+		'tanggal',
+	),
+)); ?>
