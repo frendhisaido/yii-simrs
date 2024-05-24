740fcfc6b3a6e5411bf2a7dd312aef9d1e05700e
Frendhi <frendhi.saido@outlook.com>
2024-05-24 13:53:48 +0700

    aider: Added Chart.js script tag to the main layout file.
    # Aider chat conversation:
    
    USER: I added these files to the chat: protected/views/layouts/main.php
    
    ASSISTANT: To include Chart.js in the layout, we need to add the Chart.js script tag in the `protected/views/layouts/main.php` file.
    
    Here is the *SEARCH/REPLACE* block:
    
    protected/views/layouts/main.php
    ```php
    <<<<<<< SEARCH
    	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
    =======
    	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
    	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
    >>>>>>> REPLACE
    ```

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -17,6 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/rbac.css">
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
